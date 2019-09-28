f509e0687171cdab3fbad1910a791b65415b01a2
bmurdata <32182553+bmcap@users.noreply.github.com>
2019-09-28 17:32:54 -0400

    Somehwat functional scraper

--- forumdata/forumdata/spiders/forum_scraper.py
+++ forumdata/forumdata/spiders/forum_scraper.py
@@ -8,8 +8,13 @@ class ForumScraper(scrapy.Spider):
     ]
 
     def parse(self, response):
-        page = response.url..split('-')[-1]
-
+        page = response.url.split('-')[-1]
+        #print(response.xpath('//div[@id="posts"]/text()').get())
+        
         filename = 'citydata-%s.html' % page
         with open(filename, 'wb') as f:
-            f.write(response.xpath('//'))
+            #sellist=response.xpath('//div[@id="posts"]//div[starts-with(@id,"post")]/text()').getall()
+            sellist=response.xpath('//div[@id="posts"]//div[starts-with(@id,"post") and position()=(last()-1)]').getall()
+
+            for x in sellist:
+                f.write(x.encode())
