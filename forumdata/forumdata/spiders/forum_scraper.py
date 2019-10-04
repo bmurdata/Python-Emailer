db379649d2086bb38a56d3d83eb5228b4d123669
bmurdata <32182553+bmcap@users.noreply.github.com>
2019-10-04 16:58:28 -0400

    Final Update of Forum_scraper using txt files for reading. Added ability to enter username, password, and reciever Gmail Info

--- forumdata/forumdata/spiders/forum_scraper.py
+++ forumdata/forumdata/spiders/forum_scraper.py
@@ -1,26 +1,40 @@
 import scrapy
 import smtplib, ssl
+import os
+
 class ForumScraper(scrapy.Spider):
     name="forums"
-    fn="../data/url.txt"
+    #Searches two directories up from location of forum_scraper
+    #Getting the url.txt file
+    fileTest='..\\..\\data\\url.txt'
+    fn = os.path.join(os.path.dirname(__file__), fileTest)
+
+    
 
     with open(fn,"rt") as f:
          start_urls = [ url.strip() for url in f.readlines()]
 
     def parse(self, response):
+        #Gets the -72 Page number from the link:
+        #http://www.city-data.com/forum/new-york-city/2156244-staff-analyst-trainee-questions-72.html
+
         page = (response.url.split('-')[-1]).split('.html')[0]
+        #Used in SSL email sending, defining sender email, pw, and 
+        #reciever emails.
         port=465
         smtp_server="smtp.gmail.com"
-        senderEmail="YourSener@gmail.com"
-        senderpw=input("Enter password")
-        message="Subject: {Subject} has Updated!\n\n {Lastpost}\n\nSent by your Friendly Python Emailer. https://github.com/bmurdata/Python-Emailer"
-        recieverEmail="YourReiever@gmail.com"
-
-        # filename = 'citydataLastItemEMAIL-%s.txt' % page
-    
+        senderEmail=input("Enter Gmail Email(without @gmail.com): ")+"@gmail.com"
+        print(senderEmail)
+        senderpw=input("Enter password: ")
+        message="Subject: {Subject} has Updated!\n\n New post below:\n\n{Lastpost}\n\n"+"Sent by your Friendly Python Emailer. https://github.com/bmurdata/Python-Emailer"
+        recieverEmail=input("Enter reciving Gmail Email(without @gmail.com): ")+"@gmail.com"    
         filename = 'city-%s.txt' % page
-        open(filename) #Guarentee File exists
-        filename.close()
+        #Creates and closes file where the mos recent post will be stored, if it doesn't exist
+        open(filename,'a').close()
+        #Ask user if they want to clear file for testing purposes.
+        choice=input("Do you want to clear the file where the lastpost is store?(Y/N): ")
+        if choice=="Y":
+            open(filename, 'w').close()
         
         with open(filename, 'r+') as f:
             #get some posts, return the last one.
@@ -28,11 +42,13 @@ class ForumScraper(scrapy.Spider):
             lastpost=sellist[-1]
             fline= f.readline()
             if fline != lastpost:
+                f.write(lastpost)
                 print("I found something")
+                #Login to email and send a response.
                 context=ssl.create_default_context()
                 with smtplib.SMTP_SSL(smtp_server,port,context=context) as server:
                     server.login(senderEmail,senderpw)
-                    message=message.format(Subject="City-Data",Lastpost=lastpost)
+                    message=message.format(Subject="City-Data Page "+page,Lastpost=lastpost)
                     server.sendmail(senderEmail,recieverEmail,message)
             else:
                 print("No new Posts")
