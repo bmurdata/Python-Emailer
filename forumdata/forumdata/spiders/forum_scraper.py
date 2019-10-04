bcb177aaf8a6da86edaaba117aeca9ed6ec8518e
bmurdata <32182553+bmcap@users.noreply.github.com>
2019-10-03 21:03:06 -0400

    Template version of forum-scraper-working as intended

--- forumdata/forumdata/spiders/forum_scraper.py
+++ forumdata/forumdata/spiders/forum_scraper.py
@@ -1,19 +1,38 @@
 import scrapy
-
+import smtplib, ssl
 class ForumScraper(scrapy.Spider):
     name="forums"
+    fn="../data/url.txt"
 
-    start_urls = [
-    'http://www.city-data.com/forum/new-york-city/2156244-staff-analyst-trainee-questions-72.html',
-    ]
+    with open(fn,"rt") as f:
+         start_urls = [ url.strip() for url in f.readlines()]
 
     def parse(self, response):
-        page = response.url.split('-')[-1]
-        #print(response.xpath('//div[@id="posts"]/text()').get())
+        page = (response.url.split('-')[-1]).split('.html')[0]
+        port=465
+        smtp_server="smtp.gmail.com"
+        senderEmail="YourSener@gmail.com"
+        senderpw=input("Enter password")
+        message="Subject: {Subject} has Updated!\n\n {Lastpost}\n\nSent by your Friendly Python Emailer. https://github.com/bmurdata/Python-Emailer"
+        recieverEmail="YourReiever@gmail.com"
+
+        # filename = 'citydataLastItemEMAIL-%s.txt' % page
+    
+        filename = 'city-%s.txt' % page
+        open(filename) #Guarentee File exists
+        filename.close()
         
-        filename = 'citydataLastItem-%s.html' % page
-        with open(filename, 'wb') as f:
-            #sellist=response.xpath('//div[@id="posts"]//div[starts-with(@id,"post")]/text()').getall()
+        with open(filename, 'r+') as f:
+            #get some posts, return the last one.
             sellist=response.xpath('//div[@id="posts"]//div[starts-with(@id,"post")]/text()').getall()
-            lastitem=sellist[-1]
-            f.write(lastitem.encode())
+            lastpost=sellist[-1]
+            fline= f.readline()
+            if fline != lastpost:
+                print("I found something")
+                context=ssl.create_default_context()
+                with smtplib.SMTP_SSL(smtp_server,port,context=context) as server:
+                    server.login(senderEmail,senderpw)
+                    message=message.format(Subject="City-Data",Lastpost=lastpost)
+                    server.sendmail(senderEmail,recieverEmail,message)
+            else:
+                print("No new Posts")
